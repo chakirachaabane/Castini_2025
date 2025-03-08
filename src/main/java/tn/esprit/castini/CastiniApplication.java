21dab9d8673734ee2636abadd4ca58dad00b814d
Chakira <chakirachaabane@esprit.tn>
2025-03-08 23:15:11 +0100

    First commit : Structure

---
+++ src/main/java/tn/esprit/castini/CastiniApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.castini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CastiniApplication.class, args);
+    }
+
+}
